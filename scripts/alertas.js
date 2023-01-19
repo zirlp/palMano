5fd6d785c2db659e5e95a4964b2d10f69bd50c7a
AperezA28 <lib.peal@gmail.com>
2023-01-18 19:48:14 -0600

    Se añadio libreria SweetAlert para seleccionar estados

---
+++ scripts/alertas.js
@@ -0,0 +1,42 @@
+(async() => {
+    const {value: estado} = await Swal.fire({
+        title: "Bienvenido",
+        text: "Selecciona tu Pais",
+        
+        icon: "question",
+       
+        padding: "1rem",
+       
+        backdrop: "true",
+        
+         position:"center",
+         allowOutsideClick: "true",
+         allowEscapeKey: "false",
+         allowEnterKey:"false",
+         stopKeydownPropagation: "false",
+    
+         input: "select",
+         inputPlaceholder:"Estado",
+        
+        inputOptions:{
+            Jalisco: "Jalisco",
+            Sonora: "Sonora",
+            CDMX: "CDMX",
+            Guerrero: "Guerrero",
+            Durango: "Durango",
+            Sinaloa: "Sinaloa",
+        }
+        
+      
+    });
+
+    if (estado) {
+        Swal.fire({
+            title: "Seleccionaste ${estado}", 
+        });
+    }
+})()
+
+
+
+
